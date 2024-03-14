b1601d323ff99c614f4f0b8541204a9857dff54e
Yathindra Sai Rahul <yathindrasairahul@gmail.com>
2024-03-14 06:13:52 +0000

    Created a html front-end

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-  return 'Hello World!'
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
